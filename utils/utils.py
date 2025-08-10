5f6f329faafb92ba4b151a1ac61920e54a38a65f
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-08-10 02:24:03 -0300

    divisão entre dados de treinos e testes

--- utils/utils.py
+++ utils/utils.py
@@ -11,3 +11,8 @@ x = df
 
 classes = db['Has_Hypertension'].value_counts()
 print(classes)
+
+input_train, input_test, output_train, output_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+print(f"Training set size: {len(input_train)}")
+print(f"Test set size: {len(input_test)}")
