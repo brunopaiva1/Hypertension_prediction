2792313bfd4a39fba6a8503dd1cb2b1d7fe26105
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-08-10 02:33:51 -0300

    divisão dos dados entre treino e teste

--- utils/utils.py
+++ utils/utils.py
@@ -16,3 +16,16 @@ input_train, input_test, output_train, output_test = train_test_split(x, y, test
 
 print(f"Training set size: {len(input_train)}")
 print(f"Test set size: {len(input_test)}")
+
+input_train = pd.DataFrame(input_train)
+input_train.to_csv('/home/bruno/Hypertension_prediction/dataset/train/input_train.csv', index=False)
+
+input_test = pd.DataFrame(input_test)
+input_test.to_csv('/home/bruno/Hypertension_prediction/dataset/test/input_test.csv', index=False)
+
+output_train = pd.DataFrame(output_train)
+output_train.to_csv('/home/bruno/Hypertension_prediction/dataset/train/output_train.csv', index=False)
+
+output_test = pd.DataFrame(output_test)
+output_test.to_csv('/home/bruno/Hypertension_prediction/dataset/test/output_test.csv', index=False)
+print("Data has been split and saved to CSV files.")
