3fe15d781a028f822b2799040b2d29b34fe98c04
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-08-10 02:17:53 -0300

    classe e labels

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,13 @@
 import pandas as pd
-from sklearn.nodel_selection import train_test_split
+from sklearn.model_selection import train_test_split
+
+db = pd.read_csv('/home/bruno/Hypertension_prediction/dataset/hypertension_dataset.csv')
+
+print(db.head())
+
+y = db['hypertension']
+df = db.drop(['hypertension'], axis='columns')
+x = df
+
+classes = db['hypertension'].value_counts()
+print(classes)
