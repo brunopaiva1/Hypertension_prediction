ad808980a9036236bd839459011838d1fb283bb7
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-08-10 02:51:30 -0300

    correção de erro ortografico

--- utils/balancear.py
+++ utils/balancear.py
@@ -17,4 +17,4 @@ print(output_train_balanced.value_counts())
 input_train_balanced.to_csv('/home/bruno/Hypertension_prediction/dataset/train/input_train_balanced.csv', index=False)
 output_train_balanced.to_csv('/home/bruno/Hypertension_prediction/dataset/train/output_train_balanced.csv', index=False)
 
-print("\nArquivos balanceados foram salvos com sucesso!")
+print("\nFile balanced with success!")
