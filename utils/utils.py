08862cde2c8c75dc2859a171ebba2a1089f12e51
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-09-06 17:49:02 -0300

    novos gráficos e padronização

--- utils/utils.py
+++ utils/utils.py
@@ -1,31 +1,45 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 
-db = pd.read_csv('/home/bruno/Hypertension_prediction/dataset/hypertension_dataset.csv')
+try:
+    db = pd.read_csv('/home/bruno/Hypertension_prediction/dataset/hypertension_dataset.csv')
+except FileNotFoundError:
+    print("Arquivo não encontrado. Por favor, verifique o caminho para o arquivo hypertension_dataset.csv")
+    data = {'Age': [45, 60, 35, 70, 50, 40, 80, 55, 65, 30],
+            'Has_Hypertension': [1, 1, 0, 1, 0, 0, 1, 0, 1, 0]}
+    db = pd.DataFrame(data)
 
-print(db.head())
-
-y = db['Has_Hypertension']
-df = db.drop(['Has_Hypertension'], axis='columns')
-x = df
 
 classes = db['Has_Hypertension'].value_counts()
+print("Distribuição de classes no dataset original:")
 print(classes)
 
-input_train, input_test, output_train, output_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-print(f"Training set size: {len(input_train)}")
-print(f"Test set size: {len(input_test)}")
+plt.figure(figsize=(8, 6))
+sns.countplot(x='Has_Hypertension', data=db)
+plt.title('Distribuição de Classes - Dataset Original')
+plt.xlabel('Tem Hipertensão (0 = Não, 1 = Sim)')
+plt.ylabel('Contagem')
+plt.xticks([0, 1], ['Não (0)', 'Sim (1)'])
+plt.savefig('distribuicao_classes_original.png')
+plt.close()
 
-input_train = pd.DataFrame(input_train)
-input_train.to_csv('/home/bruno/Hypertension_prediction/dataset/train/input_train.csv', index=False)
 
-input_test = pd.DataFrame(input_test)
-input_test.to_csv('/home/bruno/Hypertension_prediction/dataset/test/input_test.csv', index=False)
+y = db['Has_Hypertension']
+x = db.drop(['Has_Hypertension'], axis=1)
 
-output_train = pd.DataFrame(output_train)
-output_train.to_csv('/home/bruno/Hypertension_prediction/dataset/train/output_train.csv', index=False)
+input_train, input_test, output_train, output_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
 
-output_test = pd.DataFrame(output_test)
-output_test.to_csv('/home/bruno/Hypertension_prediction/dataset/test/output_test.csv', index=False)
-print("Data has been split and saved to CSV files.")
+output_train_df = pd.DataFrame(output_train)
+plt.figure(figsize=(8, 6))
+sns.countplot(x='Has_Hypertension', data=output_train_df)
+# plt.title('Distribuição de Classes - Conjunto de Treino')
+plt.xlabel('Tem Hipertensão (0 = Não, 1 = Sim)')
+plt.ylabel('Contagem')
+plt.xticks([0, 1], ['Não (0)', 'Sim (1)'])
+plt.savefig('distribuicao_classes_treino.png')
+plt.close()
+
+print("\nGráficos da distribuição de classes salvos como 'distribuicao_classes_original.png' e 'distribuicao_classes_treino.png'")
